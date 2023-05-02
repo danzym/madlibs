c04d6f23a3c63286928d28171bf4a7803feff9a3
Abdulkadir Celebi <abdulkadircelebi.ac@gmail.com>
2023-05-02 20:15:10 +0300

    Stlye and madlibs function created.

--- do-not-touch.js
+++ do-not-touch.js
@@ -1,9 +1,14 @@
 /**
  * DO NOT TOUCH ANY OF THE CODE BELOW HERE.
- * 
+ *
  * Or you will be very sad.
  */
-const getRawStory = () => {
-  return fetch('./story.txt')
-    .then(response => response.text());
-};
+async function getRawStory() {
+	const response = await fetch('story.txt');
+	if (!response.ok) {
+		const message = `An error has occurred: ${response.status}`;
+		throw new Error(message);
+	}
+	const story = await response.text();
+	return story;
+}
--- madlibs.js
+++ madlibs.js
@@ -1,49 +1,94 @@
-/**
- * Complete the implementation of parseStory.
- *
- * parseStory retrieves the story as a single string from story.txt
- * (I have written this part for you).
- *
- * In your code, you are required (please read this carefully):
- * - to return a list of objects
- * - each object should definitely have a field, `word`
- * - each object should maybe have a field, `pos` (part of speech)
- *
- * So for example, the return value of this for the example story.txt
- * will be an object that looks like so (note the comma! periods should
- * be handled in the same way).
- *
- * Input: "Louis[n] went[v] to the store[n], and it was fun[a]."
- * Output: [
- *  { word: "Louis", pos: "noun" },
- *  { word: "went", pos: "verb", },
- *  { word: "to", },
- *  { word: "the", },
- *  { word: "store", pos: "noun" }
- *  { word: "," }
- *  ....
- *
- * There are multiple ways to do this, but you may want to use regular expressions.
- * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
- */
+// async function getRawStory() {
+//   const response = await fetch('story.txt');
+//   if (!response.ok) {
+//     const message = `An error has occurred: ${response.status}`;
+//     throw new Error(message);
+//   }
+//   const story = await response.text();
+//   return story;
+// }
+
 function parseStory(rawStory) {
-  // Your code here.
-  return {}; // This line is currently wrong :)
+	const parsedStory = [];
+	const regex = /(\w+\[[a-z]\])\s*|([\w.,;!?]+\s*)/gi;
+	const matches = rawStory.match(regex);
+
+	matches.forEach((match) => {
+		const posMatch = match.match(/(\w+)\[([a-z])\]/i);
+		if (posMatch) {
+			const word = posMatch[1];
+			const pos = posMatch[2];
+			const posMap = { n: 'noun', v: 'verb', a: 'adjective' };
+			parsedStory.push({ word, pos: posMap[pos] });
+		} else {
+			parsedStory.push({ word: match });
+		}
+	});
+
+	return parsedStory;
 }
 
-/**
- * All your other JavaScript code goes here, inside the function. Don't worry about
- * the `then` and `async` syntax for now.
- *
- * NOTE: You should not be writing any code in the global namespace EXCEPT
- * declaring functions. All code should either:
- * 1. Be in a function.
- * 2. Be in .then() below.
- *
- * You'll want to use the results of parseStory() to display the story on the page.
- */
+function createMadLibs(parsedStory) {
+	let editViewStory = '';
+	let previewStory = '';
+	const inputIds = [];
+
+	parsedStory.forEach((element, index) => {
+		const { word, pos } = element;
+		const inputId = `input_${index}`;
+
+		if (pos) {
+			inputIds.push(inputId);
+			const inputElement = `<input type="text" id="${inputId}" class="empty" maxlength="20">`;
+			editViewStory += inputElement;
+			previewStory += `<span id="preview_${index}" class="preview"></span>`; // add class to preview span
+		} else {
+			editViewStory += word;
+			previewStory += word;
+		}
+	});
+
+	document.querySelector('.madLibsEdit').innerHTML = editViewStory;
+	document.querySelector('.madLibsPreview').innerHTML = previewStory;
+
+	inputIds.forEach((id, index) => {
+		const inputElement = document.getElementById(id);
+		const previewElement = document.getElementById(`preview_${index}`); // get preview span element
+		if (previewElement) {
+			// add check to make sure preview span exists
+			inputElement.addEventListener('input', function (event) {
+				const value = event.target.value;
+				if (inputElement.classList.contains('filled')) {
+					// check if input is filled
+					previewElement.textContent = value;
+				}
+				inputElement.className = value ? 'filled' : 'empty';
+			});
+
+			inputElement.addEventListener('focus', function () {
+				inputElement.classList.add('focused');
+			});
+
+			inputElement.addEventListener('blur', function () {
+				inputElement.classList.remove('focused');
+			});
+
+			inputElement.addEventListener('keydown', function (event) {
+				if (event.key === 'Enter') {
+					event.preventDefault();
+					const nextIndex = index + 1;
+					if (nextIndex < inputIds.length) {
+						document.getElementById(inputIds[nextIndex]).focus();
+					}
+				}
+			});
+		}
+	});
+}
+
 getRawStory()
-  .then(parseStory)
-  .then((processedStory) => {
-    console.log(processedStory);
-  });
+	.then(parseStory)
+	.then(createMadLibs)
+	.catch((error) => {
+		console.error('Error processing story:', error);
+	});
